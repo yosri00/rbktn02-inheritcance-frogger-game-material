ff6a84c256e27bec7a6609eeb540a6a8cfbd4e3f
yosri00 <max.power1212222.com>
2020-03-24 17:33:12 +0100

    not done

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.enm_speed = enm_speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,22 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.enm_speed * dt;
+
+    
+    if (this.x > 510) {
+        this.x = -50;
+        this.enm_speed = 150 + Math.floor(Math.random()*200);
+    };
+
+    if (player.x < this.x + 81 &&
+        player.x + 81 > this.x &&
+        player.y < this.y + 60 &&
+        60 + player.y > this.y) {
+        player.x = 202;
+        player.y = 405;
+    };
+};
 };
 
 // Draw the enemy on the screen, required method for game
